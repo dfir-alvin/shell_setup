4cf4ea9f1d0cd0132e8d5adf3dd1cfa445ee357c
dfir-alvin <53186297+dfir-alvin@users.noreply.github.com>
2025-05-09 16:07:14 -0500

    Update Microsoft.PowerShell_profile.ps1
    hash alias-es

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,4 +8,9 @@ function .. { cd .. }
 function ll { ls }
 function df { Get-Volume }
 function ifconfig { ipconfig }
+function md5 { Get-FileHash -Algorithm md5 $args[0]}
+function sha1 { Get-FileHash -Algorithm sha1 $args[0]}
+function sha256 { Get-FileHash -Algorithm sha256 $args[0]}
+function lgrep { Select-String -Pattern $args[0]}
+#function lgrep { Select-String -Pattern $args[0] -CaseSensitive $args[1]}
 new-alias grep findstr
